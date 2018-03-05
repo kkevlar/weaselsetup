90b802611c2578ecab1efea8015a24c9bf47cb5c
Kevin Kellar <kellar.kevin@gmail.com>
2018-03-05 14:08:07 -0800

    combineweasels method working properly intitially

--- scan.c
+++ scan.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define GCPAD_PATH "weaseltemplate.ini"
-
 void boo(char* errormsg)
 {
 	printf("ERROR: %s\n",errormsg);
@@ -15,17 +13,22 @@ void fresh(FILE* file)
 {
 	while(1)
 	{
-	char c[10];
-	fgets(c, 2, file);
-	if (c[0] == '\n')
-		break;
-}
+		char c[10];
+		fgets(c, 2, file);
+		if (c[0] == '\n')
+			break;
+	}
 }
 
-int main(void)
-{	
+int combine_weasels(char* primary_file_name,
+	char* secondary_file_name,
+	int primary_device_number,
+	int secondary_device_number,
+	char* output_file_name)
+{
 	char my_buf[8000];
-	FILE* file = fopen(GCPAD_PATH,"r");
+	FILE* file = fopen(primary_file_name,"r");
+	FILE* outfile = fopen(output_file_name,"w");
 
 	fgets(my_buf, 10, file);
 	int result = strcmp("[Profile]",my_buf);
@@ -39,20 +42,75 @@ int main(void)
 	if(result != 1)
 		boo("Malformed deviceno line.");
 
-	printf("Device Number=%d\n",deviceno);
+	printf("Primary Device Number %d -> %d\n",deviceno,primary_device_number);
 
-	//will need to loop this because EOF and newline sadddd
-	fgets(my_buf, 8000, file);
+	fprintf(outfile, "[Profile]\n");
+	fprintf(outfile, "Device = DInput/%d/Wireless Gamepad\n",primary_device_number);
+	fflush(outfile);
+	
+	printf("Moving primary controls to output file...");
+	int primary_move_line_count = 0;
+	while(1)
+	{		
+		if (fgets(my_buf, 8000, file))
+		{
+			fprintf(outfile, "%s",my_buf);
+			primary_move_line_count++;
+		}
+		else
+			break;
+	}
 
-	printf("lel %s",my_buf);
-
-	// scanf(file,"%s = `%s`",my_buf, my_buf+4000);
-
-	// if(result!= 2)
-	// 	boo("Malformed binding line.");
-
+	printf("Moved %d lines.\n",primary_move_line_count);
 
 	fclose(file);   
-    return 0;
+	fclose(outfile);
+	return 0;
 }
+
+
+int main()
+{
+	combine_weasels("weaseltemplate.ini",
+		"null",
+		0,
+		1,
+		"wcopy.ini");
+}
+// int main(void)
+// {
+// 	char my_buf[8000];
+// 	FILE* file = fopen(GCPAD_PATH,"r");
+// 	FILE* outfile = fopen("wcopy.ini","w");
+
+// 	fgets(my_buf, 10, file);
+// 	int result = strcmp("[Profile]",my_buf);
+// 	if(result)
+// 		boo("Malformed profile line.");
+
+// 	fresh(file);
+
+// 	int deviceno;
+// 	result = fscanf(file,"Device = DInput/%d/Wireless Gamepad",&deviceno);
+// 	if(result != 1)
+// 		boo("Malformed deviceno line.");
+
+// 	printf("Device Number=%d\n",deviceno);
+
+// 	fprintf(outfile, "[Profile]\n");
+// 	fprintf(outfile, "Device = DInput/%d/Wireless Gamepad\n",deviceno);
+// 	fflush(outfile);
+	
+// 	while(1)
+// 	{		
+// 		if (fgets(my_buf, 8000, file))
+// 			fprintf(outfile, "%s",my_buf);
+// 		else
+// 			break;
+// 	}
+
+// 	fclose(file);   
+// 	fclose(outfile);
+// 	return 0;
+// }
 
